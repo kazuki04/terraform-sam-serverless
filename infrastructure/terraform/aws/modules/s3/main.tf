48cd31a207f4f78f89b736d0360796e1c230609f
kazuki04 <kazukik0402@gmail.com>
2022-12-11 19:56:15 +0900

    Added public block setting to hosting bucket

--- infrastructure/terraform/aws/modules/s3/main.tf
+++ infrastructure/terraform/aws/modules/s3/main.tf
@@ -9,6 +9,15 @@ resource "aws_s3_bucket" "hosting" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "hosting" {
+  bucket = aws_s3_bucket.hosting.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "hosting" {
   bucket = aws_s3_bucket.hosting.id
   policy = data.aws_iam_policy_document.hosting.json
